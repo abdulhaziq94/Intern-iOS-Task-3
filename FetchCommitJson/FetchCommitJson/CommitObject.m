404e923014f5d58d11313d0e0a2764a923d380a4
Abdul Haziq <abdulhaziq94@gmail.com>
2017-03-27 14:25:10 +0800

    first commit

---
+++ FetchCommitJson/FetchCommitJson/CommitObject.m
@@ -0,0 +1,18 @@
+//
+//  CommitObject.m
+//  FetchCommitJson
+//
+//  Created by Abdul Haziq on 27/03/2017.
+//  Copyright © 2017 Abdul Haziq. All rights reserved.
+//
+
+#import "CommitObject.h"
+
+@implementation CommitObject
+
+@synthesize CommitMessage;
+@synthesize Name;
+@synthesize Email;
+@synthesize Date;
+
+@end
